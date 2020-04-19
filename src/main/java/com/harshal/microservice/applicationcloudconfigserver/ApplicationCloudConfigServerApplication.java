fceb4d578163f2d9f4670770311b6230d8b3e5ca
harshalbawale <harshal.bawale@gmail.com>
2020-04-19 19:41:36 +0530

    First time commit

---
+++ src/main/java/com/harshal/microservice/applicationcloudconfigserver/ApplicationCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.harshal.microservice.applicationcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationCloudConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harshal/microservice/applicationcloudconfigserver/ApplicationCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshal.microservice.applicationcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
